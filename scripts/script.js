de59395b505cae69c55601a414729248e22a1596
Leonid-Tula <76412465+Leonid-Tula@users.noreply.github.com>
2021-02-25 16:33:45 +0300

    Спринт4 v0.2 исправление замечаний ревьюера

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 // Находим форму в DOM
 let formElement = document.querySelector ('.popup');// Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let form = formElement.querySelector('.popup__container')
 let nameInput = formElement.querySelector('.popup__name-field');// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__description-field');// Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__name-field_description');// Воспользуйтесь инструментом .querySelector()
 let profileName = document.querySelector('.profile__title'); // Имя профиля
 let profileDescription = document.querySelector('.profile__description') // Описание профиля
+//let addButton = document.querySelector ('.profile__add-button');
+let editButton = document.querySelector ('.profile__edit');
+let closePopup = document.querySelector ('.popup__close-icon');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -16,21 +18,11 @@ function formSubmitHandler (evt) {
 
    
                                                 // Получите значение полей jobInput и nameInput из свойства value
-    if (nameInput.value !== "" || jobInput.value !== "") {
-        console.log('Форма отправлена');
-    };
     
-    if (nameInput.value !== ""){
-        console.log('fullname: ' + nameInput.value);
-        profileName.textContent = nameInput.value;
-    };
-
-    if (jobInput.value !== ""){
-        console.log('description: ' + jobInput.value);
-        profileDescription.textContent = jobInput.value;
-    };
+     
     // Выберите элементы, куда должны быть вставлены значения полей
-
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
     // Вставьте новые значения с помощью textContent
     formElement.classList.toggle('popup_opened')
 }
@@ -39,25 +31,24 @@ function formSubmitHandler (evt) {
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener ('submit', formSubmitHandler);
 
-//let addButton = document.querySelector ('.profile__add-button');
-let editButton = document.querySelector ('.profile__edit');
-let submitButton = document.querySelector ('.popup__submit-button');
-let closePopup = document.querySelector ('.popup__close-icon');
-
+function openForm (evt) {
+    evt.preventDefault();
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
+    formElement.classList.toggle('popup_opened')
+}
 /*addButton.addEventListener ('click', function(){
     formElement.classList.toggle('popup_opened');
     console.log ("Нажали на добавить нового");
 });
 */
 
-editButton.addEventListener ('click', function(){
-    formElement.classList.toggle('popup_opened');
-    console.log ("Нажали на редактирование");
-}); 
+editButton.addEventListener ('click', openForm)
+    //{formElement.classList.toggle('popup_opened');
+    // console.log ("Нажали на редактирование");}
+; 
 
-//submitButton.addEventListener ('click', formSubmitHandler);
-
-closePopup.addEventListener ('click', function() {
-    formElement.classList.toggle('popup_opened');
-    console.log ("Нажали на крестик");
-});
+closePopup.addEventListener ('click', openForm) 
+    //{formElement.classList.toggle('popup_opened');
+    //console.log ("Нажали на крестик");}
+;
